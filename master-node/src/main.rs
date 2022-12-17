0dc02bff8968e6197e4fb4823bed7cc668907d43
Piotr Kubicki <pkubicki44@gmail.com>
2022-12-17 22:01:40 +0000

    Router introduced

--- master-node/src/main.rs
+++ master-node/src/main.rs
@@ -3,9 +3,10 @@
 use core::fmt;
 use std::error::Error;
 use std::sync::Arc;
+use std::collections::HashMap;
 
 use tokio::net::TcpListener;
-use tokio::task::JoinHandle;
+use tokio::net::tcp::OwnedWriteHalf;
 use tokio::task;
 use tokio::sync::Mutex;
 use tokio::time::{sleep, Duration};
@@ -37,17 +38,14 @@ impl Error for WorkerConnectionError{}
 
 struct Worker {
     url: String,
-    handle: Option<JoinHandle<String>>,
     status: WorkerStatus,
 }
 
 impl Worker {
     fn new(url: String) -> Worker {
-        let handle = None;
         let status = WorkerStatus::IDLE;
         Worker {
             url,
-            handle,
             status,
         }
     }
@@ -90,29 +88,6 @@ impl MasterNode {
         }
     }
 
-    async fn register_worker(&mut self, worker_url: String) -> Result<HttpResponse, Box<dyn Error>> {
-        let worker = Worker::new(worker_url);
-        self.workers.lock().await.push(worker);
-        Ok(HttpResponse::new(
-            200,
-            "OK".to_string(),
-            "OK".to_string(),
-        ))
-    }
-
-    async fn check_workers_status(&self) -> Result<HttpResponse, Box<dyn Error>> {
-        let workers = &self.workers.lock().await;
-        let mut response = String::new();
-        for worker in workers.iter() {
-            response.push_str(&format!("URL: {} STATUS: {:?}\n", worker.url, worker.status));
-        }
-        Ok(HttpResponse::new(
-            200,
-            "OK".to_string(),
-            response,
-        ))
-    }
-
     fn workers_healthcheck(&mut self) {
         let workers = self.workers.clone();
         let client = self.client.clone();
@@ -124,60 +99,20 @@ impl MasterNode {
                 let mut workers = workers.lock().await;
                 let tasks: Vec<_> = workers.iter_mut().map(|worker| worker.healthcheck(&client)).collect();
                 join_all(tasks).await;
+
+                let mut failed_workers: Vec<usize> = Vec::new();
+                for (i, worker) in workers.iter().enumerate() {
+                    if worker.status == WorkerStatus::ERROR {
+                        failed_workers.push(i);
+                    }
+                }
+                for failed_worker in failed_workers {
+                    workers.remove(failed_worker);
+                }
                 info!("Health check done");
             }
         });
     }
-
-    fn run_task(&self, req: &HttpRequest) -> Result<HttpResponse, Box<dyn Error>> {
-        let workers = self.workers.clone();
-        let client = self.client.clone();
-
-        match &req.body {
-            Some(task) => {
-                let bucket = task.clone();
-                task::spawn(async move {
-                    info!("Running task...");
-                    let config = aws_config::load_from_env().await;
-                    let s3_client = s3::Client::new(&config);
-                    let res = s3_client.list_objects_v2().bucket(&bucket).send().await;
-
-                    match res {
-                        Ok(res) => {
-                            for obj in res.contents().unwrap_or_default() {
-                                let file = obj.key().unwrap_or_default().to_string();
-                                info!("File {file}....");
-                                let path = format!("{bucket}/{file}");
-                                let worker_index = get_idle_worker_index(workers.as_ref()).await;
-                                let mut workers = workers.as_ref().lock().await;
-                                let _ = client.post(format!("{}/task", workers[worker_index].url))
-                                    .header("content-length", path.len())
-                                    .body(path).send().await;
-                                workers[worker_index].status = WorkerStatus::RUNNING;
-                            }
-                        },
-                        Err(e) => println!("Something goes wrong! {}", e),
-                    }
-                });
-            },
-            None => info!("No data received for processing"),
-        }
-
-        Ok(HttpResponse::new(
-            200,
-            "OK".to_string(),
-            "OK".to_string(),
-        ))
-    }
-
-    async fn process_result(&self, _req: &HttpRequest) -> Result<HttpResponse, Box<dyn Error>> {
-        Ok(HttpResponse::new(
-            200,
-            "OK".to_string(),
-            "OK".to_string(),
-        ))
-    }
-
 }
 
 async fn get_idle_worker_index(workers: &Mutex<Vec<Worker>>) -> usize {
@@ -193,17 +128,128 @@ async fn get_idle_worker_index(workers: &Mutex<Vec<Worker>>) -> usize {
     }
 }
 
-async fn route(node: &mut MasterNode, req: &HttpRequest) -> Result<HttpResponse, Box<dyn Error>> {
-    match req.uri.as_deref() {
-        Some("/register") => {
-            info!("Registering");
-            let worker_url = &req.body.as_ref().unwrap();
-            node.register_worker(worker_url.to_string()).await
+fn register_worker(req: HttpRequest, write_stream: OwnedWriteHalf, node: &MasterNode) -> Result<(), Box<dyn Error>> {
+    let workers = node.workers.clone();
+    task::spawn(async move {
+        let worker_url = req.body.as_ref().unwrap();
+        let worker = Worker::new(worker_url.to_string());
+        workers.lock().await.push(worker);
+        let res = HttpResponse::new(
+            200,
+            "OK".to_string(),
+            "OK".to_string(),
+        );
+        res.encode(write_stream).await;
+    });
+
+    Ok(())
+}
+
+fn check_workers_status(_req: HttpRequest, write_stream: OwnedWriteHalf, node: &MasterNode) -> Result<(), Box<dyn Error>> {
+    let workers = node.workers.clone();
+    task::spawn(async move {
+        let workers = workers.lock().await;
+        let mut response = String::new();
+        for worker in workers.iter() {
+            response.push_str(&format!("URL: {} STATUS: {:?}\n", worker.url, worker.status));
+        }
+        let res = HttpResponse::new(
+            200,
+            "OK".to_string(),
+            response,
+        );
+        res.encode(write_stream).await;
+    });
+
+    Ok(())
+}
+
+fn run_task(req: HttpRequest, write_stream: OwnedWriteHalf, node: &MasterNode) -> Result<(), Box<dyn Error>> {
+    let workers = node.workers.clone();
+    let client = node.client.clone();
+
+    match &req.body {
+        Some(task) => {
+            let bucket = task.clone();
+            task::spawn(async move {
+                info!("Running task...");
+                let config = aws_config::load_from_env().await;
+                let s3_client = s3::Client::new(&config);
+                let res = s3_client.list_objects_v2().bucket(&bucket).send().await;
+
+                match res {
+                    Ok(res) => {
+                        for obj in res.contents().unwrap_or_default() {
+                            let file = obj.key().unwrap_or_default().to_string();
+                            info!("File {file}....");
+                            let path = format!("{bucket}/{file}");
+                            let worker_index = get_idle_worker_index(workers.as_ref()).await;
+                            let mut workers = workers.as_ref().lock().await;
+                            let _ = client.post(format!("{}/task", workers[worker_index].url))
+                                .header("content-length", path.len())
+                                .body(path).send().await;
+                            workers[worker_index].status = WorkerStatus::RUNNING;
+                        }
+                    },
+                    Err(e) => println!("Something goes wrong! {}", e),
+                }
+            });
+            task::spawn(async move {
+                let res = HttpResponse::new(
+                    200,
+                    "OK".to_string(),
+                    "OK".to_string(),
+                );
+                res.encode(write_stream).await;
+            });
         },
-        Some("/status") => node.check_workers_status().await,
-        Some("/task") => node.run_task(req),
-        Some("/result") => node.process_result(req).await,
-        _ => Ok(HttpResponse::new(404, "NOT FOUND".to_string(), "".to_string())),
+        None => info!("No data received for processing"),
+    }
+
+    Ok(())
+}
+
+fn process_result(_req: HttpRequest, write_stream: OwnedWriteHalf, node: &MasterNode) -> Result<(), Box<dyn Error>> {
+    task::spawn(async move{
+        let res = HttpResponse::new(
+            200,
+            "OK".to_string(),
+            "OK".to_string(),
+        );
+        res.encode(write_stream).await;
+    });
+
+    Ok(())
+}
+
+struct Router {
+    routes: HashMap<String, fn(HttpRequest, OwnedWriteHalf, &MasterNode) -> Result<(), Box<dyn Error>>>,
+}
+
+impl Router {
+    fn new() -> Self {
+        Router {
+            routes: HashMap::new(),
+        }
+    }
+
+    fn register(&mut self, url: String, action: fn(HttpRequest, OwnedWriteHalf, &MasterNode) -> Result<(), Box<dyn Error>>) {
+        self.routes.insert(url, action);
+    }
+
+    async fn process(&self, stream: tokio::net::TcpStream, node: &MasterNode) {
+        let (read_stream, write_stream) = stream.into_split();
+        let req = HttpRequest::decode(read_stream).await;
+        let uri = req.as_ref().unwrap().uri.as_ref();
+        let route = self.routes.get(uri.unwrap());
+        match route {
+            Some(route) => (route)(req.unwrap(), write_stream, &node),
+            None => {
+                let res = HttpResponse::new(404, "NOT_FOUND".to_string(), "NOT_FOUND".to_string());
+                res.encode(write_stream).await;
+                Ok(())
+            },
+        };
     }
 }
 
@@ -217,6 +263,12 @@ impl fmt::Display for WorkerConnectionError {
 async fn main() -> Result<(), Box<dyn Error>> {
     env_logger::init();
     let mut node = MasterNode::new();
+    let mut router = Router::new();
+    router.register("/register".to_string(), register_worker);
+    router.register("/status".to_string(), check_workers_status);
+    router.register("/task".to_string(), run_task);
+    router.register("/result".to_string(), process_result);
+
     let addr = format!("{}:{}", IP_ADDRES, PORT_NUMBER);
     let listener = TcpListener::bind(&addr).await?;
 
@@ -225,21 +277,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     loop {
         match listener.accept().await {
             Ok((stream, _)) => {
-                let (read_stream, write_stream) = stream.into_split();
-                let req = HttpRequest::decode(read_stream).await;
-                match req {
-                    Ok(req) => {
-                        let response = route(&mut node, &req).await;
-                        match response {
-                            Ok(response) => response.encode(write_stream).await,
-                            Err(e) => eprintln!("Something goes wrong {e}")
-                        }
-                    },
-                    Err(e) => {
-                        eprintln!("{e}");
-                        break;
-                    }
-                }
+                router.process(stream, &node).await;
             },
             Err(e) => {
                 eprintln!("{e}");
